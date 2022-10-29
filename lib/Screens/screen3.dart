ccad2061b32c1afd0af81d8a8a4670597471f6db
Prita2002 <pritanoviana8@gmail.com>
2022-10-29 10:14:45 +0700

    first commit

---
+++ lib/Screens/screen3.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Screen3 extends StatelessWidget {
+  const Screen3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        HeroData(
+          nim: '183200021',
+          nama: 'ahmad hizbullah akbar',
+        ),
+        HeroData(
+          nim: '183200022',
+          nama: 'Haikal',
+        ),
+      ],
+    );
+  }
+}
+
+class HeroData extends StatelessWidget {
+  const HeroData({Key? key, required this.nim, required this.nama})
+      : super(key: key);
+
+  final String nim;
+  final String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(nim),
+        SizedBox(width: 5),
+        Text(nama),
+      ],
+    );
+  }
+}
